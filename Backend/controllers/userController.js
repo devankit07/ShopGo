f81394a6aeabc88718192e378979225cc7709733
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-01-07 15:29:15 +0530

    register & verify

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -43,3 +43,58 @@ export const register = async (req, res) => {
     });
   }
 };
+
+export const verify = async (req, res) => {
+  try {
+    const authHeader = req.headers.authorization;
+    
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(400).json({ 
+        success: false,
+        message: "Authorization token is missing or invalid"
+      });
+    }
+
+    const token = authHeader.split(" ")[1];
+    let decoded;
+
+    try {
+      decoded = jwt.verify(token, process.env.SECRET_KEY);
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(400).json({
+          success: false,
+          message: "The registration Token has Expired"
+        });
+      }
+      return res.status(400).json({
+        success: false,
+        message: "Token Verification failed"
+      });
+    }
+
+    const user = await User.findById(decoded.id);
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        message: "User Not Found"
+      });
+    }
+
+    user.token = null;
+    user.isVerified = true; 
+    await user.save();
+
+    return res.status(200).json({
+      success: true,
+      message: "Email verified successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message
+    });
+  }
+};
--- Backend/emialiVerify/Verifyemail.js
+++ Backend/emialiVerify/Verifyemail.js
@@ -23,3 +23,4 @@ export const verifyEmail = (token, email) => {
     console.log(info);
   });
 };
+
