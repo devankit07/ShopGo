0faaaaa4f5c70d7e85f96320af41d70a50f17395
Ankitkumar277 <Ankitrathor272005@gmail.com>
2026-02-04 00:02:15 +0530

    fetch all products

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -1,3 +1,7 @@
+import getDataUri from "../utils/dataUri";
+import Cloudinary from "../utils/Cloudinary";
+import Product from "../models/productModel";
+
 export const addProduct = async (req, res) => {
     try {
         const { productName, productDesc, productPrice, category, brand } = req.body;
@@ -8,13 +12,44 @@ export const addProduct = async (req, res) => {
         }
 
         // //handle mulltiple img
-        // let productImage = []; 
-        // if(req.files && req.files.length > 0) {
-        //     for (let files of req.files) {
-        //        const fileUri = 
-        //     }
-        // }
+        let productImage = []; 
+        if(req.files && req.files.length > 0) {
+            for (let file of req.files) {
+               const fileUri = getDataUri(file)
+               const result = await Cloudinary.uploader.upload(fileUri,{
+                folder: "products" //cloudinary folder name
+               })
+               productImage.push({url: result.secure_url, public_id: result.public_id});
+            }
+        }
+        //create a product in db
+        const newProduct = await Product.create({
+            userId,
+            productName,
+            productDesc,
+            productPrice,
+            category,
+            brand,
+            productImage //array of objects [{url,punlic_id}]
+        })
+        return res.status(200).json({
+            scuccess:true,
+            message:"product added successfully",
+            product:newProduct
+        })
     } catch (error) {
         res.status(500).json({ message: error.message });
     }}
+
+export const getallproduct = async (_, res)=>{
+    try {
+        const product = await Product.find()
+        //yha se continue
+    } catch (error) {
+        return res.status(500).json({
+            scuccess:false,
+            message:error.message
+        })
+    }
+} 
     
